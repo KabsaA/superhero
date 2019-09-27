f01f663b99367d7cf36f8f28def6cd0a44c3114c
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-09-26 18:17:08 -0700

    now outputting Grace Hopper and 200

--- superhero.py
+++ superhero.py
@@ -1,14 +1,11 @@
 import random
 class Ability:
     def __init__(self, name, attack_strength):
-      '''Create Instance Variables:
-          name:String
-          max_damage: Integer
-       '''
+          self.name = name
+          self.attack_strength = attack_strength
        # TODO: Instantiate the variables listed in the docstring with then
        # values passed in
-      self.name = name
-      self.attack_strength = attack_strength
+      #self.attack_strength = attack_strength
     def attack(self):
       #''' Return a value between 0 and the value set by self.max_damage.'''
       # TODO: Use random.randint(a, b) to select a random attack value.
@@ -21,23 +18,22 @@ class Ability:
 
 class Armor:
     def __init__(self, name, max_block):
-        '''Instantiate instance properties.
-            name: String
-            max_block: Integer
-        '''
+    #Instantiate instance properties.
+            self.name = name
+            self.max_block = max_block
         # TODO: Create instance variables for the values passed in.
-        def block(self):
+    def block(self):
             #Return a random value between 0 and the initialized max_block strength.
-            pass
+        pass
       # TODO: This method should run Ability.attack() on every ability
       # in self.abilities and returns the total as an integer.
 class Hero:
     def __init__(self, name, starting_health=100):
-          self.abilities = []
+          self.abilities = Hero.add_ability(ability)
           armors: []
-          name: self.name
+          self.name = name
           starting_health: starting_health
-          current_health: 0
+          self.current_health = (starting_health - 0)
 
        # TODO: Initialize instance variables values as instance variables
        # (Some of these values are passed in above,
@@ -45,14 +41,15 @@ class Hero:
        # abilities and armors are lists that will contain objects that we can use
     def add_ability(self, ability):
         abilities = []
-           #''' Add ability to abilities list '''
+        #for i in ability:
+        abilities.append(ability)
+  #''' Add ability to abilities list '''
+  # TODO: Add ability object to abilities:List
 
            # TODO: Add ability object to abilities:List
     def attack(self):
         #Calculate the total damage from all ability attacks. return: total:Int
-        total = 0
-        for i in abilities:
-            total += 1
+        pass
     def defend(self,incoming_damage):
         pass
     def take_damage(self,damage):
@@ -67,12 +64,11 @@ class Hero:
 
 
 if __name__ == "__main__":
-    # If you run this file from the terminal
+        # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
     ability = Ability("Great Debugging", 50)
+    ability2 = Ability("Grging", 70)
     hero = Hero("Grace Hopper", 200)
     hero.add_ability(ability)
+    hero.add_ability(ability2)
     print(hero.abilities)
