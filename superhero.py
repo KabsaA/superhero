eb430e77e18e3f40b3cd0fd03dcc23c727e6c0da
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-09-27 16:04:20 -0700

    added Arena class and starter methods

--- superhero.py
+++ superhero.py
@@ -80,7 +80,20 @@ class Hero:
         # This method should add the number of deaths to self.deaths
         self.deaths += num_deaths
         return self.deaths
-
+      def add_weapon(self, weapon):
+        '''Add weapon to self.abilities'''
+        # TODO: This method will append the weapon object passed in as an
+        # argument to self.abilities.
+        # This means that self.abilities will be a list of
+        # abilities and weapons.
+        pass
+    def add_armor(self, armor):
+    '''Add Armor to self.armors
+        armor: Armor Object
+    '''
+    # TODO: This method will add the armor object that is passed in to
+    # the list of armor objects defined in the constructor: `self.armors`.
+    pass
 
        # TODO: Initialize instance variables values as instance variables
        # (Some of these values are passed in above,
@@ -142,21 +155,104 @@ class Hero:
             self.add_kill(1)
             self.add_deaths(1)
             print(str(opponent.name) + " wins!")
+class Arena:
+    def __init__(self):
+        '''Instantiate properties
+            team_one: None
+            team_two: None
+        '''
+        # TODO: create instance variables named team_one and team_two that
+        # will hold our teams.
+     def create_ability(self):
+        '''Prompt for Ability information.
+            return Ability with values from user Input
+        '''
+        # TODO: This method will allow a user to create an ability.
+        # Prompt the user for the necessary information to create a new ability object.
+        # return the new ability object.
+        pass
+     def create_weapon(self):
+        '''Prompt user for Weapon information
+            return Weapon with values from user input.
+        '''
+        # TODO: This method will allow a user to create a weapon.
+        # Prompt the user for the necessary information to create a new weapon object.
+        # return the new weapon object.
+        pass
+     def create_armor(self):
+        '''Prompt user for Armor information
+          return Armor with values from user input.
+        '''
+        # TODO:This method will allow a user to create a piece of armor.
+        #  Prompt the user for the necessary information to create a new armor
+        #  object.
+        #
+        #  return the new armor object with values set by user.
+        pass
+        def create_hero(self):
+        '''Prompt user for Hero information
+          return Hero with values from user input.
+        '''
+        # TODO: This method should allow a user to create a hero.
+        # User should be able to specify if they want armors, weapons, and
+        # abilities.
+        # Call the methods you made above and use the return values to build
+        # your hero.
+        #
+        # return the new hero object
+        pass
+      def build_team_one(self):
+        '''Prompt the user to build team_one '''
+        # TODO: This method should allow a user to create team one.
+        # Prompt the user for the number of Heroes on team one
+        # call self.create_hero() for every hero that the user wants to add to team one.
+        #
+        # Add the created hero to team one.
+        pass
+
+    def build_team_two(self):
+        '''Prompt the user to build team_two'''
+        # TODO: This method should allow a user to create team two.
+        # Prompt the user for the number of Heroes on team two
+        # call self.create_hero() for every hero that the user wants to add to team two.
+        #
+        # Add the created hero to team two.
+        pass
+    def team_battle(self):
+        '''Battle team_one and team_two together.'''
+        # TODO: This method should battle the teams together.
+        # Call the attack method that exists in your team objects
+        # for that battle functionality.
+        pass
+    def show_stats(self):
+        '''Prints team statistics to terminal.'''
+        # TODO: This method should print out battle statistics
+        # including each team's average kill/death ratio.
+        # Required Stats:
+        #     Declare winning team
+        #     Show both teams average kill/death ratio.
+        #     Show surviving heroes.
+        pass
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
+    arena = Arena()
+    arena.build_team_one()
+    arena.build_team_two()
+    arena.team_battle()
+    arena.show_stats()
 
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-    ability1 = Ability("Super Speed", 900)
-    print(Weapon.attack(ability1))
-    ability2 = Ability("Super Eyes", 750)
-    ability3 = Ability("Wizard Wand", 2000)
-    ability4 = Ability("Wizard Beard", 2000)
-    hero1.add_ability(ability1)
-    hero1.add_ability(ability2)
-    hero2.add_ability(ability3)
-    hero2.add_ability(ability4)
-    hero2.fight(hero1)
+    # hero1 = Hero("Wonder Woman")
+    # hero2 = Hero("Dumbledore")
+    # ability1 = Ability("Super Speed", 900)
+    # print(Weapon.attack(ability1))
+    # ability2 = Ability("Super Eyes", 750)
+    # ability3 = Ability("Wizard Wand", 2000)
+    # ability4 = Ability("Wizard Beard", 2000)
+    # hero1.add_ability(ability1)
+    # hero1.add_ability(ability2)
+    # hero2.add_ability(ability3)
+    # hero2.add_ability(ability4)
+    # hero2.fight(hero1)
